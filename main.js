8ac8afe7cd46b6ebd62bd8ec802ecded9ae3c75a
Wilfredo Ramirez <rwilfredo534@gmail.com>
2020-06-03 13:15:50 -0400

    final commit

--- main.js
+++ main.js
@@ -1,37 +1,53 @@
-
-
 function add(x, y) {
-    const result = x + y
-    return result
+  const result = x + y
+  return result
 }
 
 console.log('my add function', add(6, 8))
 
 
-
-//Per Zachs recommendation:
-
-//function multiply(x, y) {
-//Create a placeholder variable here
-//for loop goes here, make it go until y is met {
-//placeholder variable = add(x, placeholder)
-//}
-//return placeholder variable
-//}
-//multiply(5, 3)
-
 function multiply(x, y) {
-    let product = 0
+  let product = 0
 
-    for (let counter = 0; counter < y; counter += 1) {
-        console.log('counter')
-        product = add(x, product)
-        console.log('product ', product)
-    }
+  for (let counter = 0; counter < y; counter += 1) {
+    console.log('counter')
+    product = add(x, product)
+    console.log('product', product)
+  }
 
-    return product
+  return product
 }
 
-console.log('my multiplaction function', multiply(9, 7))
+console.log('my multiply function', multiply(6, 4))
 
-            
+
+function power(x, n) {
+  let result = 1
+
+  for (let counter = 0; counter < n; counter += 1) {
+    console.log('counter')
+    result = multiply(x, result)
+    console.log('result', result)
+  }
+
+  return result
+
+}
+console.log('my power function', power(3, 4))
+
+function factorial (x) {
+let result = 1
+
+for (let counter = 1; counter<=x; counter+=1) {
+  console.log(counter)
+  result= multiply (counter, result)
+  console.log('result',result)
+}
+
+return result
+
+}
+
+console.log('my factorial function', factorial (3))
+
+
